f50175eb0747c0559f42ebfa8220fc149824b974
Kayque Viana <kayque.viana@catskillet.com>
2021-10-15 16:39:19 -0300

    modificadas rotas do arquivo

--- SiteRpg/routes/web.php
+++ SiteRpg/routes/web.php
@@ -16,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RpgController;
 
 Route::get('/', [RpgController::class, 'index']);
+Route::get('/info', [RpgController::class, 'info']);
+Route::get('/bestiario', [RpgController::class, 'bestiary']);
+Route::get('/mapas', [RpgController::class, 'maps']);
+Route::get('/e1', [RpgController::class, 'e1']);
+Route::get('/e2', [RpgController::class, 'e2']);
+
+
+
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
